9d655382e676f6eaaad2a04e92c25ecce36c0cdf
Abby Lin <zhu.lin@sas.com>
2020-06-15 15:43:19 +0800

    Fixed issues

--- TryTools.py
+++ TryTools.py
@@ -14,6 +14,7 @@ import sys
 browser = webdriver.Chrome()
 accountsDic = [{'user': 'linzhu0831@163.com', 'password': 'LINzhu2565651'}, {'user': '815924867@qq.com', 'password': 'LINzhu2565651'}]
 mainURL = "https://www.jd.com"
+followedURL = "https://t.jd.com/vender/followVenderList.action?index=9"
 
 def dealWithTrySites():
     if not os.path.isfile("channel.json"):
@@ -24,7 +25,7 @@ def dealWithTrySites():
     channels_dict = json.loads(channels)
     for channel_info in channels_dict:
         print(channel_info)
-        if channel_info['channel_name'] == '生鲜美食': #--or channel_info['channel_name'] == '食品饮料'*#:
+        if channel_info['channel_name'] == '生鲜美食' or channel_info['channel_name'] == '食品饮料':
             dealWithChannel(channel_info)
 
 def dealWithChannel(channel_info):
@@ -35,15 +36,25 @@ def dealWithChannel(channel_info):
         goods_list = browser.find_element_by_id('goods-list')
         items = goods_list.find_elements_by_xpath('//li[@class="item"]')
         for item in items:
+            #时间处理
             time_stamp = item.get_attribute('end_time')
             end_date = datetime.date.fromtimestamp(int(time_stamp) / 1000)
             today = datetime.date.today()
             daysleft = end_date.__sub__(today).days
+
+            #产品名称黑白列表处理
             name = item.find_element_by_class_name('p-name').text
-            blacklist_re = '|'.join(channel_info['channel_blacklist'])
-            in_blacklist = bool(re.search(blacklist_re, name) != None)
+            name_verified = True
+            if 'channel_blacklist' in channel_info:
+                blacklist_re = '|'.join(channel_info['channel_blacklist'])
+                name_verified = not bool(re.search(blacklist_re, name) != None)
+
+            if 'channel_whitelist' in channel_info:
+                whitelist_re = '|'.join(channel_info['channel_whitelist'])
+                name_verified = bool(re.search(whitelist_re, name) != None)
+
             #只申请距离结束3天之内的产品，产品名称不能在黑名单之内
-            if not in_blacklist and daysleft < 3:
+            if name_verified and daysleft < 3:
                 #打开试用产品申请页
                 link = item.find_element_by_class_name('link').get_attribute('href')
                 if None == re.search('https:', link):
@@ -95,7 +106,12 @@ def dealWithChannel(channel_info):
                         #申请成功，退出循环
                         break
                     elif re.search('超过上限', alert_tip):
-                        sys.exit()
+                        #申请数达到上限，抛出异常
+                        raise ValueError('申请数量超过上限')
+                    elif re.search('关注上限', alert_tip):
+                        clean_followed_malls()
+                        browser.refresh()
+                        time.sleep(2)
                     else:
                         break
 
@@ -175,11 +191,25 @@ def login_with_cookies(user_dict):
         return False
 
 
+def clean_followed_malls():
+    js = 'window.open(' + '\"' + followedURL + '\"' + ')'
+    browser.execute_script(js)
+    handles = browser.window_handles
+    tab_index = len(handles)
+    browser.switch_to.window(handles[tab_index-1])
+    browser.find_element_by_xpath('//div[@class="batch-box J-batchBox"]/a[@class="btn-def batch-btn"]').click()
+    browser.find_element_by_xpath('//div[@class="batch-operate"]/span[@class="op-btn u-check"]').click()
+    browser.find_element_by_xpath('//div[@class="batch-operate"]/span[@class="op-btn u-unfollow"]').click()
+    browser.close()
+    browser.switch_to.window(tab_index - 2)
 
 if __name__ == "__main__":
     for account in accountsDic:
-        if account['user'] == '815924867@qq.com':
+        # if account['user'] == '815924867@qq.com':
+        #     continue
+        try:
+            loginJD(account)
+            print("用户：" + account['user'] + " 已登录，跳转试用列表")
+            dealWithTrySites()
+        except ValueError as err:
             continue
-        loginJD(account)
-        print("用户：" + account['user'] + " 已登录，跳转试用列表")
-        dealWithTrySites()
